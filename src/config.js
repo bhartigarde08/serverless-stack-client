2c026be49ce6011217d826f6f6f06ae4ccf4c983
Bharti Garde <bharti.garde@bluepineapple.io>
2020-12-18 11:13:23 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = {
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://sapbo63bae.execute-api.us-east-1.amazonaws.com/dev/users",
+    },
+    cognito: {
+      REGION: "ap-south-1",
+      USER_POOL_ID: "ap-south-1_6UfwrWfsV",
+      APP_CLIENT_ID: "4jar47ut29p1dc46u78br2qu2u",
+    },
+  };
+  
+  export default config;
